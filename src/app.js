a8b249e2430095a626bcdb93300267ed07306055
Jorge <jorgednavarroa@hotmail.com>
2023-11-12 14:29:54 -0300

    agregado cors

--- src/app.js
+++ src/app.js
@@ -1,22 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import reservasRoutes from "./routes/reservas.routes.js"
-
+import reservasRoutes from "./routes/reservas.routes.js";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import habitacionesRoutes from "./routes/habitaciones.route.js"
-
+import habitacionesRoutes from "./routes/habitaciones.route.js";
 
 const app = express();
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", authRoutes);
 
-
-app.use ("/api/reservas" , reservasRoutes)
-app.use ("/api/habitaciones", habitacionesRoutes);
-
+app.use("/api/reservas", reservasRoutes);
+app.use("/api/habitaciones", habitacionesRoutes);
 
 export default app;
-
