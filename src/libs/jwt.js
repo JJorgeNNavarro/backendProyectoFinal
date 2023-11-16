8fb4efe842e68bcab478b91ce9e8cf23c1f140bd
Jorge <jorgednavarroa@hotmail.com>
2023-11-15 21:06:27 -0300

    menos duracion de expiracion del token

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -2,11 +2,12 @@ import { TOKEN_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
 export function createAccesToken(payload) {
   return new Promise((resolve, reject) => {
+    //tenemos que pasar 3 claves , el payload, la clave privada , y el tiempo de expiracion
     jwt.sign(
       payload,
       TOKEN_SECRET,
       {
-        expiresIn: "1d",
+        expiresIn: "2h",
       },
       (err, token) => {
         if (err) reject(err);
