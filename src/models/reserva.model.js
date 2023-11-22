7ecbd345e349040602b2b9ffdf3d6734c4272dc7
sol lobo <lobosol62@gmail.com>
2023-11-22 12:05:55 -0300

    Se cambia modelo y servicio de reserva

--- src/models/reserva.model.js
+++ src/models/reserva.model.js
@@ -4,21 +4,24 @@ const reservaSchema = new Schema(
     {
         fechaEntrada: {
             type: Date,
-            required : true,
+            required : true
         },
         fechaSalida: {
             type: Date,
-            required : true,
+            required : true
         },
         habitacion: {
             type: Schema.Types.ObjectId,
             required: true,
-            ref: "Habitaciones",
-        },
-        usuario: {
-            type: Schema.Types.ObjectId,
-            required: true,
             ref: "Habitaciones"
+        },
+        nombre: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
         }
     }
 )
